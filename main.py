72f08f3f8e3470be7b44baa15d64d27eb4eb8f0f
Axolotl <2326311571@qq.com>
2022-06-27 21:02:13 +0800

    哦哦哦修改语句
    114514

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Thing2 = '花溪建的学校'
 
 dl1 = ['{}大家好，这里是{}，接下来小编跟大家了解一下{}。那么{}到底是什么呢？接下来一起来跟小编了解一下{}吧！\n\n'.format(Reader, Writer, Thing, Thing, Thing),
        'Hello，{}大家好，欢迎收看本期的{}，{}可以点击订阅关注我们，喜欢的话记得留言收藏和点赞哦。接下来小编跟大家来了解一下{}。那么{}到底是什么呢？接下来一起来跟小编了解一下{}吧！\n\n'.format(Reader, Writer, Reader, Thing, Thing, Thing),
-       '{}大家好，我是{}，快点订阅关注！必须点赞留言收藏！不然我就不写文章了！唔唔唔！今天来了解了解一下{}，快点给我来了解{}！\n\n'.format(Reader, Writer, Reader, Thing, Thing)]
+       '{}大家好，我是{}，快点订阅关注！必须点赞留言收藏！不然我就不写文章了！唔唔唔！今天来了解一下{}，快点给我来了解{}！\n\n'.format(Reader, Writer, Reader, Thing, Thing)]
 
 dl2 = ['{}，众所周知，{}是就是{}，那么这是为什么呢？有专家说这是金科玉律，{}是不是很神奇呢？\n\n'.format(Reader, Thing, Thing2, Thing),
        '{}，近日，一位外国小哥发现了{}，小哥首先吸了一口气，可以看到他还呼出一口气了，这还真是神奇呢，不得不说老外的思维很开阔了。\n\n'.format(Reader, Thing),
